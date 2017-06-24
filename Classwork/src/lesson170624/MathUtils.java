7ac1f75404b2636849f613d95620face6c5e685f
megmeehey <megmeehey@gmail.com>
2017-06-24 13:40:15 +0300

    Lesson 170624 Classwork. FizzBuzz task.

---
+++ Classwork/src/lesson170624/MathUtils.java
@@ -0,0 +1,15 @@
+package lesson170624;
+
+/**
+ * Created by Megmeehey on 24.06.17.
+ */
+public class MathUtils {
+    public static int max(int i, int j) {
+        if (i >= j) {
+            return i;
+        }
+        else {
+            return j;
+        }
+    }
+}
