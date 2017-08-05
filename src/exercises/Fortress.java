a7ee0dabb643aaff106be49a250472845203f9cd
megmeehey <megmeehey@gmail.com>
2017-08-05 20:51:53 +0300

    Added full CodingBat

---
+++ src/exercises/Fortress.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import java.io.IOException;
+
+public class Fortress {
+    public void openDrawbridge() throws IOException {
+
+        //        try {
+//            throw new IOException("Circle");
+//        }
+//        catch (IOException e) {
+//            System.out.println("Bla");
+//        }
+//        finally {
+//            System.out.println("Bla");
+//        }
+    }
+
+    public static void main(String[] args) {
+//        new Fortress().openDrawbridge(); // p3
+    }
+}
